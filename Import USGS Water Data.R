00a347a42aa9c22513d41925c76e071829a7dde9
Pat Brown <126992136+pattybrown@users.noreply.github.com>
2024-01-13 08:24:29 -0900

    Update Import USGS Water Data.R

--- Import USGS Water Data.R
+++ Import USGS Water Data.R
@@ -1,12 +1,19 @@
 ### Import USGS Water Data
 
 require(waterData)
+require(lubridate)
 
-fonty <- importDVs("09211200", code = "00060", stat = "00003", sdate = "2020-01-01", edate = "2020-12-31")
-fontyt <- importDVs("09211200", code = "00010", stat = "00003", sdate = "2020-01-01", edate = "2020-12-31")
-fonty$dates <- ymd(fonty$dates)
-fontyt$dates <- ymd(fontyt$dates)
-fonty <- merge(fonty, fontyt, by = "dates")
+fonty <- importDVs("09211200", #select station
+                   code = "00060", #select streamflow data
+                   stat = "00003", sdate = "2020-01-01", edate = "2020-12-31") #select data date range
+
+fontyt <- importDVs("09211200", code = "00010", #select temperature data
+                    stat = "00003", sdate = "2020-01-01", edate = "2020-12-31")
+
+fonty$dates <- ymd(fonty$dates) #format dates
+fontyt$dates <- ymd(fontyt$dates) #format dates
+
+fonty <- merge(fonty, fontyt, by = "dates") #merge dataframes
 colnames(fonty)[colnames(fonty) == "val.y"] <- "temp"
 colnames(fonty)[colnames(fonty) == "val.x"] <- "discharge"
 
